b3800bf164ba95475add1192d810f7c79e2b9829
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-03 10:23:30 +0300

    CREATES AN INSTANCE TEST CASE FOR THE PROFILE MODEL

--- phoenixgram/tests.py
+++ phoenixgram/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile, Image
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+  # Set up method
+  def setUp(self):
+    self.new_profile = Profile(image = 'profile_pict', bio = 'This is my bio')
+    
+  #Test Instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_profile, Profile))
+    
