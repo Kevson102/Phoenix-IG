27d022615e49849f4b7a55aeb3f30e6bc74e25f6
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-03 07:56:12 +0300

    INSTALLS BOOTSTRAP, CREATES BASE.HTML AND NAVBAR.HTML, AND LINKS BOOTSTRAP TO THE BASE.HTML

--- instagram/settings.py
+++ instagram/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'phoenixgram.apps.PhoenixgramConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
