f0b662f8263bacbab7094dd8fc8f8d3e3302af5d
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-03 08:48:57 +0300

    CREATES THE PROFILE MODEL AND THE IMAGE MODEL IN THE MODELS.PY FILE

--- phoenixgram/models.py
+++ phoenixgram/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  profile_photo = models.ImageField(upload_to='static/images/')
+  bio = models.TextField()
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='static/images/')
+  image_name = models.CharField(max_length=30)
+  image_caption = models.TextField()
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  like = models.IntegerField()
+  comment = models.TextField()
+  
