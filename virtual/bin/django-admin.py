7b3a0f4150cd74e5817ed4ba264a98a6eaff2625
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-05 21:27:13 +0300

    ENFORCES .GITIGNORE RULES

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kevson/Desktop/Core/Django/Independent Projects/Week 2/InstagramClone/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
