47cd5c6f9cdf6549c2b62f42e74dc7ff1d330a10
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-05 16:50:26 +0300

    CREATES THE SIGN UP FUNCTIONALITY IN THE APPLICATION

--- instagram/urls.py
+++ instagram/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('phoenixgram.urls'))
+    url(r'', include('phoenixgram.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
